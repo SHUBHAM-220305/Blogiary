57c0371a44fdb71f8099ce3468920c136f8e9fa3
SHUBHAM-220305 <shubhamk.22032005@gmail.com>
2026-02-08 19:03:02 +0530

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const dbConnect = require('./config/dbConnect');
-const userRoute = require('./routes/userRoutes');
-const blogRoute = require('./routes/blogRoutes');
-const cloudinaryConfig = require('./config/cloudinaryConfig');
-const { PORT } = require('./config/dotenv.config');
-const port = PORT || 5000; 
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./config/dbConnect");
+const userRoute = require("./routes/userRoutes");
+const blogRoute = require("./routes/blogRoutes");
+const cloudinaryConfig = require("./config/cloudinaryConfig");
+const { PORT } = require("./config/dotenv.config");
+const port = PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173/" }));
 
 app.use("/api/v1", userRoute);
 app.use("/api/v1", blogRoute);
 
 app.listen(port, () => {
-    console.log("Server Started!");
-    dbConnect();
-    cloudinaryConfig();
-})
+  console.log("Server Started!");
+  dbConnect();
+  cloudinaryConfig();
+});
