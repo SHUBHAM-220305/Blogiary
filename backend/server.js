b286d1f2b16b2a845889c9691f4ed23ef32577f7
SHUBHAM-220305 <shubhamk.22032005@gmail.com>
2026-02-09 02:33:03 +0530

    Final Changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173","https://blogiary.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
