061b7b3ea4a12e3ec1daed6085c1c5a67becdc27
SHUBHAM-220305 <shubhamk.22032005@gmail.com>
2026-02-08 19:11:18 +0530

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://localhost:5173" }));
 
 app.use("/api/v1", userRoute);
 app.use("/api/v1", blogRoute);
